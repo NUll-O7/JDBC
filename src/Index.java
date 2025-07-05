6e67d246a0b8534072d12d8808a12b2390a8005b
NUll_O7 <dhruvsonimain@gmail.com>
2025-07-06 02:23:07 +0530

    Starting out

---
+++ src/Index.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Index {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+    }
+}
